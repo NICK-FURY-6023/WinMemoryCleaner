568b04c2926b6edd8dc979a165c7568f4969058f
₦ł₵₭  ₣ɄⱤɎ <96591657+NICK-FURY-6023@users.noreply.github.com>
2024-10-18 18:54:40 +0530

    Create CallerMemberNameAttribute.cs

---
+++ src/Attribute/CallerMemberNameAttribute.cs
@@ -0,0 +1,13 @@
+﻿// Class required because of .NET 4.0 Framework
+// ReSharper disable once CheckNamespace
+namespace System.Runtime.CompilerServices
+{
+    /// <summary>
+    /// Allows you to obtain the method or property name of the caller to the method.
+    /// </summary>
+    /// <seealso cref="Attribute" />
+    [AttributeUsage(AttributeTargets.Parameter)]
+    public sealed class CallerMemberNameAttribute : Attribute
+    {
+    }
+}
